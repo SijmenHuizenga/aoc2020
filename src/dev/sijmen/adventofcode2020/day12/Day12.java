507790339922e09f6666c3ed62f55b8d4f65a07e
sijmenhuizenga <sijmenhuizenga@gmail.com>
2020-12-13 10:20:15 +0100

    day 12 part 2

--- src/dev/sijmen/adventofcode2020/day12/Day12.java
+++ src/dev/sijmen/adventofcode2020/day12/Day12.java
@@ -3,6 +3,7 @@ package dev.sijmen.adventofcode2020.day12;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -71,57 +72,64 @@ public class Day12 {
         System.out.println("Part 1: " + Math.abs(x) + Math.abs(y));
     }
 
+    static final int N = 0;
+    static final int E = 1;
+    static final int S = 2;
+    static final int W = 3;
+
     private static void part2(List<String> input) {
-        int shipX = 0; //positive=north, negative=south
-        int shipY = 0; //positive=east, negative=west
+        int shipNorth = 0; //negative = south
+        int shipEast = 0; //negative = west
+        int[] waypoint = new int[]{1, 10, 0, 0};
 
-        int waypointX = 1;
-        int waypointY = 10;
-
-        int[][] rotations = new int[][]{{-1, 1}, {-1, -1}, {1, -1}};
-
-        // north=0, east=90, south=180, west=270
-        int direction = 90;
         for (String c : input) {
             int amount = Integer.parseInt(c.substring(1));
             switch (c.charAt(0)) {
 //              Action N means to move north by the given value.
                 case 'N':
-                    waypointX += amount;
+                    waypoint[N] += amount;
                     break;
 //              Action S means to move south by the given value.
                 case 'S':
-                    waypointX -= amount;
+                    waypoint[S] += amount;
                     break;
 //              Action E means to move east by the given value.
                 case 'E':
-                    waypointY += amount;
+                    waypoint[E] += amount;
                     break;
 //              Action W means to move west by the given value.
                 case 'W':
-                    waypointY -= amount;
+                    waypoint[W] += amount;
                     break;
 //              Action L means to rotate the waypoint around the ship left (counter-clockwise) the given number of degrees.
                 case 'L':
-                    waypointX *= rotations[(amount / 90) - 1][0];
-                    waypointY *= rotations[(amount / 90) - 1][1];
+                    for(int i = 0; i < (amount/90); i++) {
+                        int first = waypoint[0];
+                        waypoint[0] = waypoint[1];
+                        waypoint[1] = waypoint[2];
+                        waypoint[2] = waypoint[3];
+                        waypoint[3] = first;
+                    }
                     break;
 //              Action R means to rotate the waypoint around the ship right (clockwise) the given number of degrees.
                 case 'R':
-                    waypointX *= rotations[3 - (amount / 90)][0];
-                    waypointY *= rotations[3 - (amount / 90)][1];
+                    for(int i = 0; i < (amount/90); i++) {
+                        int last = waypoint[3];
+                        waypoint[3] = waypoint[2];
+                        waypoint[2] = waypoint[1];
+                        waypoint[1] = waypoint[0];
+                        waypoint[0] = last;
+                    }
                     break;
 //              Action F means to move forward to the waypoint a number of times equal to the given value.
                 case 'F':
-                    System.out.println(" Moving the ship X + " + waypointX * amount);
-                    shipX += waypointX * amount;
-                    System.out.println(" Moving the ship Y + " + waypointY * amount);
-                    shipY += waypointY * amount;
+                    shipNorth += (waypoint[N] - waypoint[S]) * amount;
+                    shipEast += (waypoint[E] - waypoint[W]) * amount;
                     break;
             }
-            System.out.println("Ship: N" + shipX + "; E" + shipY + "   waypoint: N" + waypointX + "; E" + waypointY);
+            System.out.println("Ship: N" + shipNorth + "; E" + shipEast + "   waypoint: N" + Arrays.toString(waypoint));
         }
-        System.out.println("Part 2: " + Math.abs(shipX) + Math.abs(shipY));
+        System.out.println("Part 2: " + (Math.abs(shipNorth) + Math.abs(shipEast)));
     }
 
 }
