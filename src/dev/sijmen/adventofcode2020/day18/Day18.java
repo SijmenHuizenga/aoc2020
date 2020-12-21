23d8fb2cb77d86b1c4568ea4a11e5020b908905e
sijmenhuizenga <sijmenhuizenga@gmail.com>
2020-12-21 17:45:27 +0100

    Day 18, part 2

--- src/dev/sijmen/adventofcode2020/day18/Day18.java
+++ src/dev/sijmen/adventofcode2020/day18/Day18.java
@@ -20,6 +20,9 @@ public class Day18 {
 
         long result = input.stream().map(this::basicMath).map(SolvePart::getValue).reduce(Long::sum).get();
         System.out.println("Part 1: " + result);
+
+        result = input.stream().map(this::advancedMath).map(Equation::value).reduce(Long::sum).get();
+        System.out.println("Part 2: " + result);
     }
 
     static HashMap<Character, LongBinaryOperator> operators = new HashMap<>();
@@ -66,7 +69,7 @@ public class Day18 {
             if (c == ')') {
                 return new SolvePart(i + 1, value);
             }
-            if(value == 0) {
+            if (value == 0) {
                 value = c - '0';
                 continue;
             }
@@ -79,5 +82,148 @@ public class Day18 {
         return new SolvePart(equation.length(), value);
     }
 
+    public Equation advancedMath(String input) {
+        return advancedMathParser(input.replace(" ", "")).e;
+    }
 
+    public JavaPleaseGiveMeTuples advancedMathParser(String input) {
+        char operation = 0;
+        Equation equation = null;
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '+' || c == '*') {
+                operation = c;
+                continue;
+            }
+
+            Equation e;
+
+            if(c == '(') {
+                JavaPleaseGiveMeTuples x = advancedMathParser(input.substring(i + 1));
+                e = new CombinedEquation(x.e);
+                i += x.eqCharLength;
+            } else if (c == ')') {
+                if(operation != 0) {
+                    throw new IllegalStateException("BUG");
+                }
+                return new JavaPleaseGiveMeTuples(equation, i+1);
+            } else {
+                e = new Constant(c - '0');
+            }
+
+            if (equation == null) {
+                equation = e;
+                continue;
+            }
+
+            if (operation == '*') {
+                equation = new MulEquation(equation, e);
+                operation = 0;
+            } else if (operation == '+') {
+                equation = summy(equation, e);
+                operation = 0;
+            }
+        }
+        return new JavaPleaseGiveMeTuples(equation, input.length());
+    }
+
+    private Equation summy(Equation left, Equation right) {
+        if (left instanceof MulEquation) {
+            // A*B => +C => (A*B)+C => A*(B+C)
+            return new MulEquation(((MulEquation) left).left, new SumEquation(((MulEquation) left).right, right));
+        } else {
+            return new SumEquation(left, right);
+        }
+    }
+
+    class JavaPleaseGiveMeTuples {
+        Equation e;
+        int eqCharLength;
+
+        public JavaPleaseGiveMeTuples(Equation e, int eqCharLength) {
+            this.e = e;
+            this.eqCharLength = eqCharLength;
+        }
+
+        public Equation getE() {
+            return e;
+        }
+    }
+
+    abstract class Equation {
+        abstract long value();
+    }
+
+    class Constant extends Equation {
+        long value;
+
+        public Constant(long value) {
+            this.value = value;
+        }
+
+        public long value() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "" + value;
+        }
+    }
+
+    class SumEquation extends Equation {
+        Equation left, right;
+
+        public SumEquation(Equation left, Equation right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        long value() {
+            return left.value() + right.value();
+        }
+
+        @Override
+        public String toString() {
+            return "(" + left + "+" + right + ')';
+        }
+    }
+
+    class MulEquation extends Equation {
+        Equation left, right;
+
+        public MulEquation(Equation left, Equation right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        long value() {
+            return left.value() * right.value();
+        }
+
+        @Override
+        public String toString() {
+            return "(" + left + "*" + right + ')';
+        }
+    }
+
+    class CombinedEquation extends Equation {
+
+        Equation inside;
+
+        public CombinedEquation(Equation inside) {
+            this.inside = inside;
+        }
+
+        @Override
+        long value() {
+            return inside.value();
+        }
+
+        @Override
+        public String toString() {
+            return inside.toString();
+        }
+    }
 }
